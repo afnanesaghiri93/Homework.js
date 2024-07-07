adb5c0bb446ff189c3b3167a50bdd2d30a5a22ba
afnanesaghiri93 <saghiriafnane1993@gmail.com>
2024-07-06 23:24:29 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -20,7 +20,7 @@ for (let i = 0; i <= 200; i++ ) {// Write a for loop that will log only the even
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+//Using a switch statement
 for (let i = 1; i <= 100; i++) {//1-Write a javascript application that logs all numbers from 1 - 100
     switch (true) {
         case (i % 3 === 0 && i % 5 === 0):
@@ -36,3 +36,53 @@ for (let i = 1; i <= 100; i++) {//1-Write a javascript application that logs all
             console.log(i);
     }
 }
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+//Use the following arrays to answer the questions below (name,species ,age, hometown):
+// You should be modifying the elements by accessing them. It is up to you which methods to use.
+// I will use the splice method
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+plantee.splice(2, 1, plantee[2] + 1); // Change Plantee's age is now 5001
+wolfy.splice(3,1,"Gotham City" ); //Change Wolfy's hometown is now "Gotham City"
+dart.splice(4,0,"Hawkins");//Give D'Art a second hometown by adding "Hawkins"
+wolfy.splice(0,1,"Gameboy");//Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+
+console.log(plantee); // ["Plantee", "plant", 5001, "Mordor"]
+console.log(wolfy); // ["Gameboy", "wolf", 16, "Gotham City"]
+console.log(dart); // ["D'Art", "Demogorgan Dog", 2, "Upside Down", "Hawkins"]
+console.log(porgee); // ["Porgee", "Porg", 186, "Ahch-To"]
+console.log(sharky);//[ 'Sharky', 'shark', 20, 'Left Coast' ]
+
+
+
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
