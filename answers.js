8461df527d71921ce2b6af5fe98a82e4c492d9c6
afnanesaghiri93 <saghiriafnane1993@gmail.com>
2024-07-06 22:47:45 -0400

    Easy Going answered

---
+++ answers.js
@@ -0,0 +1,16 @@
+// Homework for 07/06/2024
+// Name: Afnane Saghiri
+
+// Part II: JavaScript Reps
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+for (let i = 1; i <= 20; i++) {//Write a for loop that will log the numbers 1 through 20.
+    console.log(i);
+}
+
+
+for (let i = 0; i <= 200; i++ ) {// Write a for loop that will log only the even numbers in 0 through 200.
+    if  (i % 2 === 0)
+        console.log(i);
+}
